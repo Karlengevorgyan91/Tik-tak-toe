b8dd3e6db99b8fd42314ea861f460d6db2192cf0
Karlen Gevorgyan <gevorgyankarlen@gmail.com>
2021-11-17 14:38:15 +0400

    Add implementation for put method

--- src/main/java/tictactoe_game/Main.java
+++ src/main/java/tictactoe_game/Main.java
@@ -1,13 +1,15 @@
 package tictactoe_game;
 
+import tictactoe_game.model.Coordinate;
 import tictactoe_game.model.Element;
 import tictactoe_game.model.Grid;
-import tictactoe_game.model.Player;
 
 public class Main {
-  public static Grid put(Player player, Element element, Grid grid) {
-
-    return null;
-  }
-
+    public static Grid put(Element element, Grid grid, Coordinate coordinate) {
+        String putElement = element.name();
+        Integer x = coordinate.x;
+        Integer y = coordinate.y;
+        grid.grid[x][y] = putElement;
+        return grid;
+    }
 }
