2806119cd2cf60ea15a6ec8c58cd569770395621
Karlen Gevorgyan <gevorgyankarlen@gmail.com>
2021-11-18 21:30:12 +0400

    Create method createEmpatyArray

--- src/main/java/tictactoe_game/model/Grid.java
+++ src/main/java/tictactoe_game/model/Grid.java
@@ -3,27 +3,39 @@ package tictactoe_game.model;
 import java.util.Arrays;
 
 public class Grid {
-    public final String[][] grid;
+  public final String[][] grid;
 
-    public Grid(String[][] grid) {
-        this.grid = grid;
+  public Grid(String[][] grid) {
+    this.grid = grid;
+  }
+
+  public static String[][] createEmpatyArray() {
+    String[][] emptyArray = new String[3][3];
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        emptyArray[i][j] = Element.EMPTY.name();
+      }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-            
-        if (!(o instanceof Grid)) {
-            return false;
-        }
+    return emptyArray;
+  }
 
-        Grid grid = (Grid) o;
-
-        Boolean check = (this.grid == null && grid.grid == null)
-                || (this.grid != null && Arrays.deepEquals(this.grid, grid.grid));
-
-        return check;
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
     }
+
+    if (!(o instanceof Grid)) {
+      return false;
+    }
+
+    Grid grid = (Grid) o;
+
+    Boolean check = (this.grid == null && grid.grid == null)
+        || (this.grid != null && Arrays.deepEquals(this.grid, grid.grid));
+
+    return check;
+  }
 }
