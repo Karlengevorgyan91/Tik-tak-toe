4fe113f6e3cb84ee2a0ecbc02f9889a3b0c55644
karlengevorgyan_qt <gevorgyankarlen@gmail.com>
2021-12-20 02:43:48 -0800

    Rewrite `CellEmptyTest`

--- src/test/java/tictactoe_game/CellEmptyTest.java
+++ src/test/java/tictactoe_game/CellEmptyTest.java
@@ -1,91 +1,38 @@
 package tictactoe_game;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import tictactoe_game.implementation.PutImplementation;
+import tictactoe_game.model.Cell;
 import tictactoe_game.model.Coordinate;
-import tictactoe_game.model.Grid;
+import tictactoe_game.model.Element;
 
 public class CellEmptyTest {
   @Test
-  @DisplayName("Check that the cell on coordinate '0,0' is empty")
+  @DisplayName("The cell on coordinate '0,0' must be empty")
   public void cell_must_be_empty_on_coordinate_0_0() {
-    Coordinate coord = new Coordinate(0, 0);
-    Grid emptyGrid = Grid.emptyGrid();
+    Cell cell = new Cell(new Coordinate(0, 0), Element.EMPTY);
 
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
+    assertTrue(PutImplementation.is_the_cell_empty(cell));
   }
 
   @Test
-  @DisplayName("Check that the cell on coordinate '0,1' is empty")
-  public void cell_must_be_empty_on_coordinate_0_1() {
-    Coordinate coord = new Coordinate(0, 1);
-    Grid emptyGrid = Grid.emptyGrid();
+  @DisplayName("The cell on coordinate '0,0' must be `X`")
+  public void cell_must_be_x_on_coordinate_0_0() {
+    Cell cell = new Cell(new Coordinate(0, 0), Element.X);
 
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
+    assertFalse(PutImplementation.is_the_cell_empty(cell));
   }
 
   @Test
-  @DisplayName("Check that the cell on coordinate '0,2' is empty")
-  public void cell_must_be_empty_on_coordinate_0_2() {
-    Coordinate coord = new Coordinate(0, 2);
-    Grid emptyGrid = Grid.emptyGrid();
+  @DisplayName("The cell on coordinate '0,0' must be `O`")
+  public void cell_must_be_o_on_coordinate_0_0() {
+    Cell cell = new Cell(new Coordinate(0, 0), Element.O);
 
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
-  }
-
-  @Test
-  @DisplayName("Check that the cell on coordinate '1,0' is empty")
-  public void cell_must_be_empty_on_coordinate_1_0() {
-    Coordinate coord = new Coordinate(1, 0);
-    Grid emptyGrid = Grid.emptyGrid();
-
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
-  }
-
-  @Test
-  @DisplayName("Check that the cell on coordinate '1,1' is empty")
-  public void cell_must_be_empty_on_coordinate_1_1() {
-    Coordinate coord = new Coordinate(1, 1);
-    Grid emptyGrid = Grid.emptyGrid();
-
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
-  }
-
-  @Test
-  @DisplayName("Check that the cell on coordinate '1,2' is empty")
-  public void cell_must_be_empty_on_coordinate_1_2() {
-    Coordinate coord = new Coordinate(0, 0);
-    Grid emptyGrid = Grid.emptyGrid();
-
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
-  }
-
-  @Test
-  @DisplayName("Check that the cell on coordinate '2,0' is empty")
-  public void cell_must_be_empty_on_coordinate_2_0() {
-    Coordinate coord = new Coordinate(2, 0);
-    Grid emptyGrid = Grid.emptyGrid();
-
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
-  }
-
-  @Test
-  @DisplayName("Check that the cell on coordinate '2,1' is empty")
-  public void cell_must_be_empty_on_coordinate_2_1() {
-    Coordinate coord = new Coordinate(2, 1);
-    Grid emptyGrid = Grid.emptyGrid();
-
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
-  }
-
-  @Test
-  @DisplayName("Check that the cell on coordinate '2,2' is empty")
-  public void cell_must_be_empty_on_coordinate_2_2() {
-    Coordinate coord = new Coordinate(2, 2);
-    Grid emptyGrid = Grid.emptyGrid();
-
-    assertTrue(Main.cell_is_empty(emptyGrid, coord));
+    assertFalse(PutImplementation.is_the_cell_empty(cell));
   }
 }
