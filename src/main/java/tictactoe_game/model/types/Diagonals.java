be1de2c60d6f1b70c2566e72d85f4835d5f8b8cc
KarlenGevorgyan-QT <karlen@KarlenGevorgyan-QTs-MacBook-Pro.local>
2021-12-28 14:51:47 +0400

    [Feature] Decide whether the game is over or not

---
+++ src/main/java/tictactoe_game/model/types/Diagonals.java
@@ -0,0 +1,8 @@
+package tictactoe_game.model.types;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tictactoe_game.model.Element;
+
+public class Diagonals extends ArrayList<List<Element>> {}
